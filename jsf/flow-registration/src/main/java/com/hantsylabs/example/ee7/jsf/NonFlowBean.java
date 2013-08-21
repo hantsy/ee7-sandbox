9380afe10adf051d14e635640c7856724a417b55
hantsy <hantsy@gmail.com>
2013-08-21 09:46:12 +0800

    update the flow example.

---
+++ jsf/flow-registration/src/main/java/com/hantsylabs/example/ee7/jsf/NonFlowBean.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.hantsylabs.example.ee7.jsf;
+
+import javax.enterprise.inject.Model;
+
+/**
+ *
+ * @author hantsy
+ */
+@Model
+public class NonFlowBean {
+    
+    public String go(){
+        return "/nonFlow";
+    }
+    
+}
